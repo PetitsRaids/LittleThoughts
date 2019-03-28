efa1048f2021fd9719d3e1048626ff4b017422ba
Raids <petits_raids@qq.com>
2019-03-28 19:59:46 +0800

    fundamental UI

---
+++ app/src/main/java/com/example/littlethoughts/HeadFrameLayout.java
@@ -0,0 +1,38 @@
+package com.example.littlethoughts;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class HeadFrameLayout extends Fragment {
+
+    private ImageView cover, headPortrait;
+
+    private TextView userName, email;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.user_head, container, false);
+        cover = view.findViewById(R.id.cover);
+        headPortrait = view.findViewById(R.id.head_portrait);
+        userName = view.findViewById(R.id.username);
+        email = view.findViewById(R.id.email);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        cover.setImageDrawable(getResources().getDrawable(R.drawable.linmeimei));
+        GlideApp.with(HeadFrameLayout.this).load(R.drawable.xiao_ice).circleCrop().into(headPortrait);
+        userName.setText(R.string.user_name);
+        email.setText(R.string.email);
+    }
+}
--- app/src/main/java/com/example/littlethoughts/MainActivity.java
+++ app/src/main/java/com/example/littlethoughts/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.littlethoughts;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        Toolbar toolbar;
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.menu);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(Gravity.START);
+                break;
+            default:
+                break;
+        }
+        return true;
     }
 }
